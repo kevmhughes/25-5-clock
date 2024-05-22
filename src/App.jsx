e450690ed54331996c8a0cc001dbc1fe4d39ee6e
kevmhughes <kevhughes24@hotmail.com>
2024-05-22 17:04:02 +0200

    Add code required until User Story 18, and update styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,70 +3,102 @@ import "./App.css";
 
 function App() {
   const [breakLength, setBreakLength] = useState(5);
-  const [sessionLength, setSessionLength] = useState(25)
-  const [timeLeftMinutes, setTimeLeftMinutes] = useState(25)
-  const [timeLeftSeconds, setTimeLeftSeconds] = useState(60)
+  const [sessionLength, setSessionLength] = useState(25);
+  const [timeLeftMinutes, setTimeLeftMinutes] = useState(25);
+  const [timeLeftSeconds, setTimeLeftSeconds] = useState(60);
 
   let timerSeconds;
   if (timeLeftSeconds == 60) {
-    timerSeconds = "00"
+    timerSeconds = "00";
   } else if (timeLeftSeconds < 60) {
-    timerSeconds = timeLeftSeconds
+    timerSeconds = timeLeftSeconds;
   }
-
 
   const handleStartStop = () => {
-    console.log("start_stop")
-  }
+    console.log("start_stop");
+  };
 
   const handleReset = () => {
-    console.log("reset")
+    console.log("reset");
     // timer should be stopped
-    setBreakLength(5)
-    setSessionLength(25)
-  }
+    setBreakLength(5);
+    setSessionLength(25);
+  };
 
   const handleBreakDecrement = () => {
-    setBreakLength(breakLength - 1)
+    setBreakLength(breakLength - 1);
     if (breakLength == 0) {
-      setBreakLength(0)
-    }
-  }
+      setBreakLength(0);
+    } 
+  };
 
   const handleBreakIncrement = () => {
-    setBreakLength(breakLength + 1)
-  }
+    setBreakLength(breakLength + 1);
+    if (breakLength == 60) {
+      setBreakLength(60);
+    }
+  };
+
+  const handleSessionDecrement = () => {
+    setSessionLength(sessionLength - 1);
+    if (sessionLength == 0) {
+      setSessionLength(0);
+    }
+  };
+
+  const handleSessionIncrement = () => {
+    setSessionLength(sessionLength + 1);
+    if (sessionLength == 60) {
+      setSessionLength(60);
+    }
+  };
 
   return (
     <>
-      <div>
+      <div className="app-container">
+        <h1>25 + 5 Clock</h1>
         <div className="break-and-session-length-container">
-        <div className="break-outer-container">
-        <div id="break-label">Break Length</div>
-        <div className="break-inner-container">
-        <div onClick={handleBreakDecrement} id="break-decrement">break -</div>
-        <div id="break-length">{breakLength}</div>
-        <div onClick={handleBreakIncrement} id="break-increment">break +</div>
-        </div>
-        </div>
-        <div>
-        <div id="session-label">Session Length</div>
-        <div>
-        <div onClick={""} id="session-decrement"></div>
-        <div id="session-length">{sessionLength}</div>
-        <div onClick={""} id="session-increment"></div>
-        </div>
-        </div>
+          <div className="break-outer-container">
+            <div id="break-label" className="break-length-labels">Break Length</div>
+            <div className="break-inner-container">
+              <div onClick={handleBreakDecrement} id="break-decrement">
+              &#8681;
+              </div>
+              <div id="break-length" className="break-length-numbers">{breakLength}</div>
+              <div onClick={handleBreakIncrement} id="break-increment">
+              &#8679;
+              </div>
+            </div>
+          </div>
+          <div className="session-outer-container">
+            <div id="session-label" className="break-length-labels">Session Length</div>
+            <div>
+              <di className="session-inner-container">
+                <div onClick={handleSessionDecrement} id="session-decrement">
+                &#8681;
+                </div>
+                <div id="session-length" className="break-length-numbers">{sessionLength}</div>
+                <div onClick={handleSessionIncrement} id="session-increment">
+                &#8679;
+                </div>
+              </di>
+            </div>
+          </div>
         </div>
 
-
-        <div>
-          <div id="timer-label">Session</div>
-          <div id="time-left">{timeLeftMinutes}:{timerSeconds}</div>
+        <div className="clock-container">
+          <div id="timer-label" className="timer-label">Session</div>
+          <div id="time-left" className="time-left">
+            {timeLeftMinutes}:{timerSeconds}
+          </div>
         </div>
         <div>
-          <div id="start_stop" onClick={handleStartStop}>start/stop</div>
-          <div id="reset" onClick={handleReset}>reset</div>
+          <div id="start_stop" onClick={handleStartStop}>
+            start/stop
+          </div>
+          <div id="reset" onClick={handleReset}>
+            reset
+          </div>
           <div></div>
         </div>
       </div>
