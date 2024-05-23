99491d72ff470f02ec9b7deb5a88651fb81e16cb
kevmhughes <kevhughes24@hotmail.com>
2024-05-23 15:07:33 +0200

    Adds more logic to the app

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,126 @@ function App() {
   const [timeLeftMinutes, setTimeLeftMinutes] = useState(25);
   const [timeLeftSeconds, setTimeLeftSeconds] = useState(60);
 
-  let timerSeconds;
-  if (timeLeftSeconds == 60) {
-    timerSeconds = "00";
-  } else if (timeLeftSeconds < 60) {
-    timerSeconds = timeLeftSeconds;
+  // sets seconds to 60
+  let seconds = timeLeftSeconds;
+  // setInterval variables
+  let ticker;
+  let ticker2;
+
+
+  /*  reduces seconds of the timer by one every 1000 ms (1 sec), puts a zero before numbers (0)9 and below & resets seconds to 60 when the textContent of the seconds field reaches 00 */
+  function reduceSeconds() {
+    let secondsField = document.querySelector("#time-left-seconds");
+    seconds -= 1;
+    secondsField.textContent = seconds;
+
+    // puts a zero before numbers (0)9 and below
+    if (seconds < 10) {
+      secondsField.textContent = "0" + seconds;
+    }
+
+    // resets seconds to 60 when the textContent of the seconds field reaches 00
+    if (secondsField.textContent == "00") {
+      seconds = timeLeftSeconds;
+      secondsField.textContent = "00";
+    }
   }
 
+  // reduces timer of the clock by one every 60,000 ms (60 secs)
+  function reduceMinutes() {
+    let minutes = document.querySelector("#time-left-minutes").textContent;
+    minutes -= 1;
+    document.querySelector("#time-left-minutes").textContent = minutes;
+  }
+
+ // is invoked when the timer reaches 0:00 & sets timer to previously chosen session length
+  function stopTheTimer() {
+    clearInterval(ticker);
+    clearInterval(ticker2);
+    ticker = null;
+    ticker2 = null;
+    document.querySelector("#time-left-minutes").textContent =
+      sessionLength;
+    document.querySelector("#time-left-seconds").textContent =
+      "00";
+  }
+
+   // is invoked when the user presses reset & sets the timer to 25:00
+  function resetTheTimer() {
+    clearInterval(ticker);
+    clearInterval(ticker2);
+    ticker = null;
+    ticker2 = null;
+    document.querySelector("#time-left-minutes").textContent =
+      "25";
+    document.querySelector("#time-left-seconds").textContent =
+      "00";
+  }
+
+  // stops the timer when it reaches 0:00
+  function stopTheTimerAt00() {
+    if (
+      document.querySelector("#time-left-minutes").textContent == "0" &&
+      document.querySelector("#time-left-seconds").textContent == "00"
+    ) {
+      stopTheTimer()
+    }
+  }
+
+  // starts seconds timer & stops the timer on click
+  function startTimer() {
+    reduceSeconds();
+    stopTheTimerAt00()
+  }
+
+  // starts minutes timer on click
+  function startTimerTwo() {
+    reduceMinutes();
+  }
+
+  // reduces the minutes by one on starting the timer, so that 25:00 becomes 24:59 after one second and not 25:59
+  function reduceMinutesByOneOnStartTimer() {
+    if (
+      document.querySelector("#time-left-minutes").textContent ==
+        timeLeftMinutes &&
+      document.querySelector("#time-left-seconds").textContent == "00"
+    ) {
+      document.querySelector("#time-left-minutes").textContent =
+        timeLeftMinutes - 1;
+    }
+  }
+
+  // handles onClick of the start/stop button
   const handleStartStop = () => {
-    console.log("start_stop");
+    setTimeout(reduceMinutesByOneOnStartTimer, 1000);
+
+    if (!ticker) {
+      ticker = setInterval(startTimer, 1000);
+    } else if (ticker) {
+      clearInterval(ticker);
+      ticker = null;
+    }
+
+    if (!ticker2) {
+      ticker2 = setInterval(startTimerTwo, 60000);
+    } else if (ticker2) {
+      clearInterval(ticker2);
+      ticker2 = null;
+    }
   };
 
+    // handles resetting of the timer to the default session length (25) and to the default break length (5) onClick of the reset button
   const handleReset = () => {
-    console.log("reset");
-    // timer should be stopped
     setBreakLength(5);
     setSessionLength(25);
+    resetTheTimer()
   };
 
   const handleBreakDecrement = () => {
     setBreakLength(breakLength - 1);
     if (breakLength == 0) {
       setBreakLength(0);
-    } 
+    }
   };
 
   const handleBreakIncrement = () => {
@@ -41,17 +138,24 @@ function App() {
 
   const handleSessionDecrement = () => {
     setSessionLength(sessionLength - 1);
-    if (sessionLength == 0) {
-      setSessionLength(0);
+    setTimeLeftMinutes(timeLeftMinutes - 1);
+    if (timeLeftMinutes == 1) {
+      setTimeLeftMinutes(1);
+    }
+    if (sessionLength == 1) {
+      setSessionLength(1);
     }
   };
 
   const handleSessionIncrement = () => {
     setSessionLength(sessionLength + 1);
+    setTimeLeftMinutes(timeLeftMinutes + 1);
     if (sessionLength == 60) {
       setSessionLength(60);
     }
   };
+
+  // https://www.freecodecamp.org/learn/front-end-development-libraries/front-end-development-libraries-projects/build-a-25--5-clock
 
   return (
     <>
@@ -59,44 +163,55 @@ function App() {
         <h1>25 + 5 Clock</h1>
         <div className="break-and-session-length-container">
           <div className="break-outer-container">
-            <div id="break-label" className="break-length-labels">Break Length</div>
+            <div id="break-label" className="break-length-labels">
+              Break Length
+            </div>
             <div className="break-inner-container">
               <div onClick={handleBreakDecrement} id="break-decrement">
-              &#8681;
+                &#8681;
               </div>
-              <div id="break-length" className="break-length-numbers">{breakLength}</div>
+              <div id="break-length" className="break-length-numbers">
+                {breakLength}
+              </div>
               <div onClick={handleBreakIncrement} id="break-increment">
-              &#8679;
+                &#8679;
               </div>
             </div>
           </div>
           <div className="session-outer-container">
-            <div id="session-label" className="break-length-labels">Session Length</div>
+            <div id="session-label" className="break-length-labels">
+              Session Length
+            </div>
             <div>
-              <di className="session-inner-container">
+              <div className="session-inner-container">
                 <div onClick={handleSessionDecrement} id="session-decrement">
-                &#8681;
+                  &#8681;
                 </div>
-                <div id="session-length" className="break-length-numbers">{sessionLength}</div>
+                <div id="session-length" className="break-length-numbers">
+                  {sessionLength}
+                </div>
                 <div onClick={handleSessionIncrement} id="session-increment">
-                &#8679;
+                  &#8679;
                 </div>
-              </di>
+              </div>
             </div>
           </div>
         </div>
 
         <div className="clock-container">
-          <div id="timer-label" className="timer-label">Session</div>
+          <div id="timer-label" className="timer-label">
+            Session
+          </div>
           <div id="time-left" className="time-left">
-            {timeLeftMinutes}:{timerSeconds}
+            <span id="time-left-minutes">{timeLeftMinutes}</span><span></span>
+            :<span id="time-left-seconds">00</span>
           </div>
         </div>
         <div>
-          <div id="start_stop" onClick={handleStartStop}>
+          <div id="start_stop" className="start-stop" onClick={handleStartStop}>
             start/stop
           </div>
-          <div id="reset" onClick={handleReset}>
+          <div id="reset" className="reset" onClick={handleReset}>
             reset
           </div>
           <div></div>
